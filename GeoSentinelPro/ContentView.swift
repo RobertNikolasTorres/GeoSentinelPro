15e7ac9bc18c291bdf73ed05fc430c1b828d3d21
robert <robert.nikolas.torres@gmail.com>
2025-10-26 15:43:39 -0400

    Initial Commit

---
+++ GeoSentinelPro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GeoSentinelPro
+//
+//  Created by Robert Nikolas Torres on 10/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GeoSentinelPro/GeoSentinelProApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoSentinelProApp.swift
+//  GeoSentinelPro
+//
+//  Created by Robert Nikolas Torres on 10/26/25.
+//
+
+import SwiftUI
+
+@main
+struct GeoSentinelProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
